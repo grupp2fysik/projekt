76d8491af333bcb6ab902783ca97c33b34ca9b8e
Tilda <tiled262@student.liu.se>
2026-04-02 12:43:25 +0000

    entropy_per_metalspace i thermodynamics.py ger delta entropi för 0 <= x <= 1

--- src/berakningsfysik/subsystem1/thermodynamics.py
+++ src/berakningsfysik/subsystem1/thermodynamics.py
@@ -1,7 +1,24 @@
 """Enkla termodynamiska hjälpfunktioner."""
 
+import math
+
 def entropy_at_endpoint(x: float) -> float:
     """Returnerar 0 i ändpunkterna x=0 och x=1."""
     if x == 0 or x == 1:
         return 0.0
     return 1.0
+
+def entropy_per_metalspace(x, n):
+    """Returnerar entropin per metallplats (eV/(atom*K))
+    x = komposition
+    n = antal metallplatser (2 för TiAlN)"""
+
+    if n < 0 or x < 0 or x > 1:
+        raise Exception("Invalid input. Only valid arguments are 0 <= x <= 1 and n > 0.")
+
+    KB = 8.6173*10**(-5) # Boltzmanns konstant (eV/K)
+
+    if entropy_at_endpoint(x) == 1:
+        return (-KB*(x*math.log(x) + (1-x)*math.log(1-x))/n)
+    else: 
+        return entropy_at_endpoint(x)
--- tests/unit/test_thermodynamics.py
+++ tests/unit/test_thermodynamics.py
@@ -1,5 +1,6 @@
+import pytest
 from berakningsfysik.subsystem1.thermodynamics import entropy_at_endpoint
-
+from berakningsfysik.subsystem1.thermodynamics import entropy_per_metalspace
 
 def test_entropy_is_zero_at_x_zero():
     assert entropy_at_endpoint(0) == 0.0
@@ -7,3 +8,30 @@ def test_entropy_is_zero_at_x_zero():
 
 def test_entropy_is_zero_at_x_one():
     assert entropy_at_endpoint(1) == 0.0
+
+
+def test_entropy_per_metalspace_is_zero_at_x_one():
+    assert entropy_per_metalspace(1, 2) == 0.0
+
+
+def test_entropy_per_metalspace_is_zero_at_x_zero():
+    assert entropy_per_metalspace(0, 2) == 0.0
+
+
+def test_entropy_per_metalspace_invalid_input():
+    """Ogiltiga argument bör lyfta undantag"""
+    with pytest.raises(Exception):
+        entropy_per_metalspace(-1, 2)
+    with pytest.raises(Exception):
+        entropy_per_metalspace(5, 9)
+    with pytest.raises(Exception):
+        entropy_per_metalspace(0.5, 0)
+    with pytest.raises(Exception):
+        entropy_per_metalspace(0.5, -1)
+
+
+def test_entropy_per_metalspace_valid_input():
+    assert entropy_per_metalspace(0.5, 2) == pytest.approx(2.9865286*10**(-5))
+    assert entropy_per_metalspace(1/3, 1) == pytest.approx(5.485033542*10**(-5))
+
+
