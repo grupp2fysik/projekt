1afaf82c127cfdb1b7a498224b6ac41ad73b1e9e
Tilda <tiled262@student.liu.se>
2026-04-02 15:22:03 +0000

    fler test

--- tests/unit/test_spinodal_functions.py
+++ tests/unit/test_spinodal_functions.py
@@ -1,6 +1,15 @@
 import pytest
+from berakningsfysik.subsystem1.thermodynamics import check_if_valid_x
 from berakningsfysik.subsystem1.spinodal_functions import entropy_second_derivative
 
 def test_entropy_second_derivative_valid_input():
     assert entropy_second_derivative(0.5) == pytest.approx(-4.0*8.6173*10**(-5))
+    assert entropy_second_derivative(1/3) == pytest.approx(-4.5*8.6173*10**(-5))
+
+def test_entropy_second_derivative_invalid_input():
+    """Bör kasta undantag om x är ogiltigt"""
+    with pytest.raises(Exception):
+        entropy_second_derivative(1.2)
+    with pytest.raises(Exception):
+        entropy_second_derivative(-0.2)
 
