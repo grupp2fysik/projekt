8c3783b3dc4e8f3c1fefa05758c2bfaf635e36df
Tilda <tiled262@student.liu.se>
2026-04-13 12:56:43 +0000

    __init__.py i enthalpy_interpolation

--- src/berakningsfysik/subsystem1/binodal_loop.py
+++ src/berakningsfysik/subsystem1/binodal_loop.py
@@ -3,17 +3,25 @@ och hitta kompositioner till binodalkurvan."""
 
 import pandas as pd
 
-df = pd.DataFrame(
-    [[1, 2, 3], [4, 5, 3], [7, 8, 6], [9, 3, 10]],
-    index=[0.2, 0.4, 0.6, 0.8],
-    columns=["GT1", "GT2", "GT3"],
-)
+from enthalpy_interpolation.interpolation import RedlichKisterModel
+
+def main():
+    model = RedlichKisterModel.fit(x=x, hmix=y, order=args.order)
+    T = [0, 50, 100, 150, 200]
+    df = pd.DataFrame(
+      [[1, 2, 3], [4, 5, 3], [7, 8, 6], [9, 3, 10]],
+     index=[0.2, 0.4, 0.6, 0.8],
+     columns=["GT1", "GT2", "GT3"],
+    )
 
 # använd df.iloc[:, start:slut] för att plocka ut G-kolumnerna ur
 # den stora dataframen från termoberäkningarna
 
 # för varje T: hitta konvext hölje - hitta fasett/tangent - hitta motsvarande x_a x_b -
 # lägg x_a x_b i en array
-for G in df.columns:
-    data = df.loc[:, G]
-    
+    for G in df.columns:
+        data = df.loc[:, G]
+        print(data)
+
+if __name__ == "__main__":
+    main()
