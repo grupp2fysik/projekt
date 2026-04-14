6e1b81f36321a003f90f416a735e3da89dee6714
Tilda <tiled262@student.liu.se>
2026-04-14 07:57:46 +0000

    mer data i dataframe.csv (Gibbs, andraderivata)

--- src/berakningsfysik/subsystem1/build_dataframe.py
+++ src/berakningsfysik/subsystem1/build_dataframe.py
@@ -1,13 +1,76 @@
+"""Denna fil sparar interpolerad deltaH, deltaS och deltaG (för alla temp)
+i en csv-fil som enkelt kan läsas till en pandas dataframe med kommando
+pd.read_csv("dataframe.csv")"""
+
 import pandas as pd
 import numpy as np
 import argparse
 from enthalpy_interpolation.interpolation import RedlichKisterModel, build_enthalpy_dataframe
 from thermodynamics import entropy_per_atom
+from spinodal_functions import entropy_second_derivative
 
-columns = ["Index", "deltaH", "deltaS"]
+columns = ["x", "deltaH", "deltaS"]
 temps = [50, 150, 250, 350]
+num_of_inter_points = 500
+n = 2
 
 def write_file():
+
+    x_interpolation = np.linspace(0.0, 1.0, num_of_inter_points)
+    model = find_model()
+    H_interpolated = find_deltaH(model, x_interpolation)
+
+    # skriver alla kolumner samt interpolationspunkter
+    with open("dataframe.csv", "w", newline="") as file:
+
+        file.write(columns[0])
+
+        for column in columns[1:]:
+            file.write(","+column)
+
+        for i in range(len(temps)):
+            file.write(",deltaG_T"+str(i))
+
+        file.write("\n")
+
+        for x in x_interpolation:
+            file.write(str(x)+",\n")
+
+    file.close()
+
+    write_data("deltaH", H_interpolated)
+    write_data("deltaS", find_deltaS(2, x_interpolation))
+    write_data("d2deltaG", find_d2G(model, x_interpolation, 300, n))
+
+    for i in range(len(temps)):
+        write_data("deltaG_T"+str(i), find_deltaG(temps[i]))
+        
+
+def write_data(column, data_array):
+    """Skriver datan i "data_array"
+    under "column" i csv-filen.
+    column = sträng från columns (t.ex "deltaH")
+    data = vektor med data till kolumnen"""
+    df = pd.read_csv("dataframe.csv")
+    df[column] = data_array
+    df.to_csv("dataframe.csv", index=False)
+
+def find_deltaS(n, x_interpolated):
+    """Hittar interpolerad deltaS_mix 
+    n = atomer per metallplats"""
+    deltaS = []
+    for x in x_interpolated:
+        deltaS.append(entropy_per_atom(x, n))
+    return np.array(deltaS)
+
+def find_deltaG(T):
+    df = pd.read_csv("dataframe.csv")
+    delta_S = df["deltaS"]
+    delta_H = df["deltaH"]
+    deltaG = delta_H - T*delta_S
+    return deltaG
+
+def find_model():
     parser = argparse.ArgumentParser(description="Skapa dataframe.")
     parser.add_argument(
         "data_dir",
@@ -26,6 +89,7 @@ def write_file():
         default=3,
         help="Ordning på Redlich-Kister-polynomet. Standard: 3",
     )
+
     args = parser.parse_args()
 
     df = build_enthalpy_dataframe(args.data_dir, glob_pattern=args.glob)
@@ -33,53 +97,24 @@ def write_file():
     x = df["x"].to_numpy()
     y = df["H_mix_eV_per_atom"].to_numpy()
 
-    model = RedlichKisterModel.fit(x=x, hmix=y, order=args.order)
+    return RedlichKisterModel.fit(x=x, hmix=y, order=args.order)
 
-    x_grid = np.linspace(0.0, 1.0, 5)
-    H_grid = model.hmix(x_grid)
-
+def find_deltaH(model, x_interpolation):
+    """Hämtar interpolerade deltaH_mix
+    x_interpolation = array med interpolationspunkter (använd np.linspace(...))"""
     
-    # skapa alla kolumner samt index/interpolationspunkter
-    with open("dataframe.csv", "w", newline="") as file:
-        file.write(columns[0])
-        for column in columns[1:]:
-            file.write(","+column)
-        for i in range(len(temps)):
-            file.write(",deltaG_T"+str(i))
-        file.write("\n")
-        for x in x_grid:
-            file.write(str(x)+",\n")
-    file.close()
+    return model.hmix(x_interpolation)
 
-    write_data("deltaH", H_grid)
-    write_data("deltaS", find_deltaS(2, x_grid))
+def find_d2G(model, x_interpolation, T, n):
+    """returnerar vektor med 
+    andraderivatan av deltaG"""
+    d2deltaH = model.d2(x_interpolation)[1:-1]
+    d2deltaS = []
+    for x in x_interpolation[1:-1]:
+        d2deltaS.append(entropy_second_derivative(x, n))
 
-    for i in range(len(temps)):
-        write_data("deltaG_T"+str(i), find_deltaG(temps[i]))
-        
-
-def write_data(column, data_array):
-    """column = sträng från columns (t.ex deltaH)
-    data = vektor med data till kolumnen"""
-    df = pd.read_csv("dataframe.csv")
-    print(df)
-    df[column] = data_array
-    print(df)
-    df.to_csv("dataframe.csv", index=False)
-
-def find_deltaS(n, x_grid):
-    deltaS = []
-    for x in x_grid:
-        deltaS.append(entropy_per_atom(x, n))
-    print(deltaS)
-    return np.array(deltaS)
-
-def find_deltaG(T):
-    df = pd.read_csv("dataframe.csv")
-    delta_S = df["deltaS"]
-    delta_H = df["deltaH"]
-    deltaG = delta_H - T*delta_S
-    return deltaG
+    d2deltaG = np.insert((d2deltaH - T*np.array(d2deltaS)), 0, np.nan)
+    return np.append(d2deltaG, [np.nan])
 
 if __name__ == "__main__":
     write_file()
