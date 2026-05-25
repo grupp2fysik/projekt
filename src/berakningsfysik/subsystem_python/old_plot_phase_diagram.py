c66c28d12ca43880811436892236f0c6f54ea3a5
Tilda <tiled262@student.liu.se>
2026-05-25 09:33:43 +0000

    plot_phase_diagram.py -> old_plot_phase_diagram.py

---
+++ src/berakningsfysik/subsystem_python/old_plot_phase_diagram.py
@@ -0,0 +1,95 @@
+"""
+Denna fil använder datan i curves.csv för 
+att plotta fasdiagrammet.
+"""
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from build_dataframe import temps
+from find_phase_curves_0 import columns
+
+
+def main():
+    """
+    Plottar fasdiagrammet baserat på data i curves.csv.
+    """
+
+    print( "Plottar fasdiagram.")
+    df = pd.read_csv("curves.csv")
+
+    fig = plt.figure()
+
+    plot_curve(df, "binodal", columns[1], columns[2])
+    plot_curve(df, "spinodal", columns[3], columns[4])
+
+    plt.xlim(0, 1)
+    plt.ylim(0,)
+    plt.xlabel("x")
+    plt.ylabel("T [K]")
+    plt.title("Fasdiagram")
+    fig.legend()
+
+    plt.savefig("plots/phase_diagrams/phase_diagram")
+
+
+def plot_curve(df, curve_type, column1, column2):
+    """
+    Sätter ihop kolumnerna column1 och column2 
+    (t.ex xa och xb)
+    och plottar mot temperaturen.
+    df är dataframe från curves.csv
+    curve_type är "spinodal" eller "binodal".
+    """
+
+    line_styles = {"spinodal": "r--", "binodal": "b-"}
+
+    main_list1, max_num_elements1 = return_comps_main_list(df, column1)
+    main_list2, max_num_elements2 = return_comps_main_list(df, column2)
+
+    main_list2.reverse()
+    max_num_elements = max(max_num_elements1, max_num_elements2)
+
+    for j in range(0, max_num_elements):
+
+            x_values = []
+            y_values =[]
+
+            for index, comps_list in enumerate(main_list1):
+                if len(comps_list) > j:
+                    if not np.isnan(comps_list[j]):
+                        x_values.append(comps_list[j])
+                        y_values.append(temps[index])
+                
+            for index, comps_list in enumerate(main_list2):
+                if len(comps_list) > j:
+                    if not np.isnan(comps_list[j]):
+                        x_values.append(comps_list[j])
+                        y_values.append(temps[-index-1])
+                
+            plt.plot(x_values, y_values, line_styles[curve_type], label = curve_type)
+
+
+def return_comps_main_list(df, column):
+    """
+    Tar en kolumn från curves.csv, t.ex "xa",
+    och omvandlar den från sträng till en nästlad lista
+    (som ser ut som kolumnen i csv-filen). 
+    Returnerar listan och längden på den av de inre
+    listorna som är längst.
+    """
+
+    comps_main_list = []
+    max_num_elements = 1
+    for index in df.index:
+            
+        comps_list = turn_string_to_list(df[column][index])
+        comps_main_list.append(comps_list)
+        if len(comps_list) > max_num_elements:
+            max_num_elements = len(comps_list)
+
+    return comps_main_list, max_num_elements
+
+
+if __name__ == "__main__":
+    main()
